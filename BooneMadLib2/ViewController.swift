2f2595ed8577304f58f9f3354d8a605e722a0c76
D'Kairi Boone <d.boone@setonhill.edu>
2023-03-19 16:53:07 -0400

    Ready

--- BooneMadLib2/ViewController.swift
+++ BooneMadLib2/ViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var Name: UITextField!
+    
+    @IBOutlet var Sport: UITextField!
+    
+    @IBOutlet var Number: UITextField!
+    
+    @IBOutlet var Food: UITextField!
+    
+    @IBOutlet var FriendName: UITextField!
+    
+    
+    
+    @IBOutlet var MadLib: UILabel!
+    
+    
+    @IBAction func CreateButton(_ sender: UIButton) {
+        MadLib.text = Name.text! + " played in his/her " + Sport.text! + " game and he/she wore the number " + Number.text! + ". After the game he/she went to eat " + Food.text! + " with his/her friend " + FriendName.text! + "."
+        
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
